4e4f158bdb456f338a3c706ab8e08eeecbade5af
iqbaltz <mhdiqbaltan@gmail.com>
2023-09-19 00:42:33 +0700

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.scss";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import SEO from "../next-seo.config";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
